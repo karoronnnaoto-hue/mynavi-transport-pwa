cd174214e61247fac286e5d2269b8aead1b5ef48
karoronnnaoto-hue <karoronnnaoto@gmail.com>
2026-07-19 21:17:50 +0900

    Build prefecture map from rectangular blocks

--- docs/sw.js
+++ docs/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "transport-search-v9";
+const CACHE = "transport-search-v10";
 const ASSETS = ["./", "index.html", "style.css", "app.js", "manifest.webmanifest", "data/jobs.json"];
 
 self.addEventListener("install", (event) => {
